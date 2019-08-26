fc3a73e7809d9424aa8c1b340c04b7235e97f7e8
JoseLuisgtz <luisxv@hotmail.com>
2019-08-26 14:32:42 -0500

    Modificacion de mensaje

--- ConsoleHolaGit/ConsoleHolaGit/Program.cs
+++ ConsoleHolaGit/ConsoleHolaGit/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleHolaGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo con Git");
+            Console.WriteLine("Hola mundo con Git. Y antes de clonar");
         }
     }
 }
